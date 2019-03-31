ed2a5e86ee6077a644600ad8451d678cbb8d7602
Kip Cole <kipcole9@gmail.com>
2019-03-31 11:33:58 +0800

    Add tests for start/end years

---
+++ test/beginning_ending_test.exs
@@ -0,0 +1,12 @@
+defmodule Cldr.Calendar.BeginsEnds.Test do
+  use ExUnit.Case
+
+  test "The US fiscal calendar for fiscal year start and end years" do
+    assert Cldr.Calendar.start_end_gregorian_years(2019, %Cldr.Calendar.Config{month: 10}) == {2018, 2019}
+  end
+
+  test "The UK fiscal calendar for fiscal year start and end years" do
+    assert Cldr.Calendar.start_end_gregorian_years(2019, %Cldr.Calendar.Config{month: 4}) == {2019, 2020}
+  end
+
+end
