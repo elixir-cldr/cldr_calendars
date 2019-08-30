e8f387247a17b033b80f017d967b1a37ee327dd9
Kip Cole <kipcole9@gmail.com>
2019-08-31 00:42:15 +0800

    Add time difference to duration calculations

--- lib/cldr/calendar/duration.ex
+++ lib/cldr/calendar/duration.ex
@@ -1,9 +1,32 @@
 defmodule Cldr.Calendar.Duration do
-  defstruct [:year, :month, :day]
+  defstruct [:year, :month, :day, :hour, :minute, :second, :microsecond]
+
+  @microseconds_in_second 1000000
+  @microseconds_in_day 86400000000
 
   def duration(%{calendar: calendar} = from, %{calendar: calendar} = to) do
+    time_diff = time_duration(from, to)
+    date_diff = date_duration(from, to)
+
+    if time_diff < 0 do
+      back_one_day(date_diff, calendar) |> add(@microseconds_in_day + time_diff)
+    else
+      date_diff |> add(time_diff)
+    end
+  end
+
+  def time_duration(%{hour: _, minute: _, second: _, microsecond: _, calendar: calendar} = from,
+      %{hour: _, minute: _, second: _, microsecond: _, calendar: calendar} = to) do
+    Time.diff(to, from, :microsecond)
+  end
+
+  def time_duration(to, from) do
+    0
+  end
+
+  def date_duration(%{calendar: calendar} = from, %{calendar: calendar} = to) do
     if Date.compare(from, to) in [:gt] do
-      raise ArgumentError, "From date must be less than or equal to to date"
+      raise ArgumentError, "`from` date must be before or equal to `to` date"
     end
 
     %{year: year1, month: month1, day: day1} = from
@@ -28,6 +51,46 @@ defmodule Cldr.Calendar.Duration do
       end
 
     %__MODULE__{year: year_diff, month: month_diff, day: day_diff}
+  end
+
+  def back_one_day(date_diff, calendar) do
+    back_one_day(date_diff, :day, calendar)
+  end
+
+  def back_one_day(%{day: day} = date_diff, :day, calendar) do
+    %{date_diff | day: day - 1}
+    |> back_one_day(:month, calendar)
+  end
+
+  def back_one_day(%{year: year, month: month, day: day} = date_diff, :month, calendar) when day < 1 do
+    %{date_diff | month: month - 1}
+    |> back_one_day(:year, calendar)
+  end
+
+  def back_one_day(%{year: year, month: month, day: day} = date_diff, :month, calendar) do
+    date_diff
+  end
+
+  def back_one_day(%{year: year, month: month, day: day} = date_diff, :year, calendar) when month < 1 do
+    diff = %{date_diff | year: year - 1}
+    diff = if diff.month < 1, do: %{diff | month: calendar.months_in_year(year)}, else: diff
+    diff = if diff.day < 1, do: %{diff | day: calendar.days_in_month(year, diff.month)}, else: diff
+    diff
+  end
+
+  def back_one_day(%{year: year, month: month, day: day} = date_diff, :year, calendar) do
+    date_diff
+  end
+
+  def add(duration, microseconds) do
+    {seconds, microseconds} = Cldr.Math.div_mod(microseconds, @microseconds_in_second)
+    {hours, minutes, seconds} = :calendar.seconds_to_time(seconds)
+
+    duration
+    |> Map.put(:hour, hours)
+    |> Map.put(:minute, minutes)
+    |> Map.put(:second, seconds)
+    |> Map.put(:microsecond, microseconds)
   end
 
   # The difference in years is adjusted if the
--- test/duration_test.exs
+++ test/duration_test.exs
@@ -5,31 +5,31 @@ defmodule Cldr.Calendar.Duration.Test do
 
   test "durations" do
     assert Duration.duration(~D[2019-01-01], ~D[2019-12-31]) ==
-      %Duration{year: 0, month: 11, day: 30}
+      %Duration{year: 0, month: 11, day: 30, hour: 0, microsecond: 0, minute: 0, second: 0}
 
     assert Duration.duration(~D[2019-01-31], ~D[2019-02-01]) ==
-      %Duration{year: 0, month: 0, day: 1}
+      %Duration{year: 0, month: 0, day: 1, hour: 0, microsecond: 0, minute: 0, second: 0}
 
     assert Duration.duration(~D[2019-12-31], ~D[2020-01-01]) ==
-      %Duration{year: 0, month: 0, day: 1}
+      %Duration{year: 0, month: 0, day: 1, hour: 0, microsecond: 0, minute: 0, second: 0}
 
     assert Duration.duration(~D[2019-01-31], ~D[2020-01-01]) ==
-      %Duration{year: 0, month: 11, day: 1}
+      %Duration{year: 0, month: 11, day: 1, hour: 0, microsecond: 0, minute: 0, second: 0}
 
     assert Duration.duration(~D[2019-05-27], ~D[2019-08-30]) ==
-      %Duration{year: 0, month: 3, day: 3}
+      %Duration{year: 0, month: 3, day: 3, hour: 0, microsecond: 0, minute: 0, second: 0}
 
     assert Duration.duration(~D[2000-05-01], ~D[2019-12-31]) ==
-      %Duration{year: 19, month: 7, day: 30}
+      %Duration{year: 19, month: 7, day: 30, hour: 0, microsecond: 0, minute: 0, second: 0}
 
     assert Duration.duration(~D[2000-12-01], ~D[2019-01-31]) ==
-      %Duration{year: 18, month: 1, day: 30}
+      %Duration{year: 18, month: 1, day: 30, hour: 0, microsecond: 0, minute: 0, second: 0}
 
     assert Duration.duration(~d[2000-12-01 Gregorian], ~d[2019-01-31 Gregorian]) ==
-      %Duration{year: 18, month: 1, day: 30}
+      %Duration{year: 18, month: 1, day: 30, hour: 0, microsecond: 0, minute: 0, second: 0}
 
     assert Duration.duration(~d[2000-12-01 CSCO], ~d[2019-01-07 CSCO]) ==
-      %Duration{year: 18, month: 1, day: 6}
+      %Duration{year: 18, month: 1, day: 6, hour: 0, microsecond: 0, minute: 0, second: 0}
   end
 
 end
