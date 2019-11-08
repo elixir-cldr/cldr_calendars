ed9fd30fb8adcc0c956518e1d31e418993fb237e
Kip Cole <kipcole9@gmail.com>
2019-11-08 11:45:57 +0100

    Fix test names

--- test/period_sequence_test.exs
+++ test/period_sequence_test.exs
@@ -30,18 +30,18 @@ defmodule Cldr.Calendar.PeriodSequenceTest do
     end
   end
 
-  test "an decending sequence of months in a week calendar follow each other" do
+  test "a descending sequence of months in a week calendar follow each other" do
     {:ok, d} = Date.new(2030, 1, 1, C454)
     m = Cldr.Calendar.Interval.month(d)
     Enum.reduce 1..1000, m, fn _i, m ->
       m2 = Cldr.Calendar.previous(m, :month)
-      IO.puts("Month: #{inspect m}; Previous: #{inspect m2}"
+      IO.puts("Month: #{inspect m}; Previous: #{inspect m2}")
       assert Cldr.Calendar.date_to_iso_days(m2.last) + 1 == Cldr.Calendar.date_to_iso_days(m.first)
       m2
     end
   end
 
-  test "an decending sequence of weeks in a week calendar follow each other" do
+  test "a descending sequence of weeks in a week calendar follow each other" do
     {:ok, d} = Date.new(2030, 1, 1, C454)
     m = Cldr.Calendar.Interval.week(d)
     Enum.reduce 1..1000, m, fn _i, m ->
