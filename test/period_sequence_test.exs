899d6cc5b7b748febee614ba4958a1b528cffc15
Kip Cole <kipcole9@gmail.com>
2019-11-08 11:56:39 +0100

    Expand test coverage for next/previous sequence checking

--- test/period_sequence_test.exs
+++ test/period_sequence_test.exs
@@ -10,44 +10,54 @@ defmodule Cldr.Calendar.PeriodSequenceTest do
       weeks_in_month: [4, 5, 4]
   end
 
-  test "an ascending sequence of months in a week calendar follow each other" do
-    {:ok, d} = Date.new(1980, 1, 1, C454)
-    m = Cldr.Calendar.Interval.month(d)
-    Enum.reduce 1..1000, m, fn _i, m ->
-      m2 = Cldr.Calendar.next(m, :month)
-      assert Cldr.Calendar.date_to_iso_days(m.last) + 1 == Cldr.Calendar.date_to_iso_days(m2.first)
-      m2
-    end
+  defmodule C544 do
+    use Cldr.Calendar.Base.Week,
+      day_of_week: 1,
+      first_or_last: :first,
+      min_days_in_first_week: 7,
+      month_of_year: 1,
+      weeks_in_month: [5, 4, 4]
   end
 
-  test "an ascending sequence of weeks in a week calendar follow each other" do
-    {:ok, d} = Date.new(1980, 1, 1, C454)
-    m = Cldr.Calendar.Interval.week(d)
-    Enum.reduce 1..1000, m, fn _i, m ->
-      m2 = Cldr.Calendar.next(m, :week)
-      assert Cldr.Calendar.date_to_iso_days(m.last) + 1 == Cldr.Calendar.date_to_iso_days(m2.first)
-      m2
+  for calendar <- [C454, C544] do
+    test "an ascending sequence of months in #{inspect calendar} follow each other" do
+      {:ok, d} = Date.new(1980, 1, 1, unquote(calendar))
+      m = Cldr.Calendar.Interval.month(d)
+      Enum.reduce 1..1000, m, fn _i, m ->
+        m2 = Cldr.Calendar.next(m, :month)
+        assert Cldr.Calendar.date_to_iso_days(m.last) + 1 == Cldr.Calendar.date_to_iso_days(m2.first)
+        m2
+      end
     end
-  end
 
-  test "a descending sequence of months in a week calendar follow each other" do
-    {:ok, d} = Date.new(2030, 1, 1, C454)
-    m = Cldr.Calendar.Interval.month(d)
-    Enum.reduce 1..1000, m, fn _i, m ->
-      m2 = Cldr.Calendar.previous(m, :month)
-      IO.puts("Month: #{inspect m}; Previous: #{inspect m2}")
-      assert Cldr.Calendar.date_to_iso_days(m2.last) + 1 == Cldr.Calendar.date_to_iso_days(m.first)
-      m2
+    test "an ascending sequence of weeks in #{inspect calendar} follow each other" do
+      {:ok, d} = Date.new(1980, 1, 1, unquote(calendar))
+      m = Cldr.Calendar.Interval.week(d)
+      Enum.reduce 1..1000, m, fn _i, m ->
+        m2 = Cldr.Calendar.next(m, :week)
+        assert Cldr.Calendar.date_to_iso_days(m.last) + 1 == Cldr.Calendar.date_to_iso_days(m2.first)
+        m2
+      end
     end
-  end
 
-  test "a descending sequence of weeks in a week calendar follow each other" do
-    {:ok, d} = Date.new(2030, 1, 1, C454)
-    m = Cldr.Calendar.Interval.week(d)
-    Enum.reduce 1..1000, m, fn _i, m ->
-      m2 = Cldr.Calendar.previous(m, :week)
-      assert Cldr.Calendar.date_to_iso_days(m2.last) + 1 == Cldr.Calendar.date_to_iso_days(m.first)
-      m2
+    test "a descending sequence of months in #{inspect calendar} follow each other" do
+      {:ok, d} = Date.new(2030, 1, 1, unquote(calendar))
+      m = Cldr.Calendar.Interval.month(d)
+      Enum.reduce 1..1000, m, fn _i, m ->
+        m2 = Cldr.Calendar.previous(m, :month)
+        assert Cldr.Calendar.date_to_iso_days(m2.last) + 1 == Cldr.Calendar.date_to_iso_days(m.first)
+        m2
+      end
+    end
+
+    test "a descending sequence of weeks in #{inspect calendar} follow each other" do
+      {:ok, d} = Date.new(2030, 1, 1, unquote(calendar))
+      m = Cldr.Calendar.Interval.week(d)
+      Enum.reduce 1..1000, m, fn _i, m ->
+        m2 = Cldr.Calendar.previous(m, :week)
+        assert Cldr.Calendar.date_to_iso_days(m2.last) + 1 == Cldr.Calendar.date_to_iso_days(m.first)
+        m2
+      end
     end
   end
 end
