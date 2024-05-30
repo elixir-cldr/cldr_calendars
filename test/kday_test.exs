811eb628d030fe88d23608e59fe8d39dcdda9db5
Kip Cole <kipcole9@gmail.com>
2024-05-31 09:02:01 +1000

    Add additional kday tests

--- test/kday_test.exs
+++ test/kday_test.exs
@@ -4,5 +4,11 @@ defmodule Calendar.Kday.Test do
   test "nth kday for k-day == day of date" do
     assert Cldr.Calendar.Kday.nth_kday(~D[2022-01-01], 1, 5) == ~D[2022-01-07]
     assert Cldr.Calendar.Kday.nth_kday(~D[2022-01-01], -1, 5) == ~D[2021-12-31]
+    assert Cldr.Calendar.Kday.nth_kday(~D[2024-01-01], 3, 1) == ~D[2024-01-15]
+    assert Cldr.Calendar.Kday.nth_kday(~D[2024-02-01], 3, 1) == ~D[2024-02-19]
+  end
+
+  test "last kday" do
+    assert Cldr.Calendar.Kday.last_kday(~D[2024-05-30], 1) == ~D[2024-05-27]
   end
 end
