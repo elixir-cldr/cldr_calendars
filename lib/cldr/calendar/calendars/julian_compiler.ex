143853638e7c5c6ecb8f366bc3929dae1f99eee0
Kip Cole <kipcole9@gmail.com>
2025-09-24 12:34:39 +1000

    Fix days_in_month/2 and month/2 for Julian calendars

--- lib/cldr/calendar/calendars/julian_compiler.ex
+++ lib/cldr/calendar/calendars/julian_compiler.ex
@@ -111,9 +111,7 @@ defmodule Cldr.Calendar.Julian.Compiler do
       end
 
       def days_in_year(year) do
-        first_day = date_to_iso_days(year, @new_year_starting_month, @new_year_starting_day)
-        first_day_next_year = date_to_iso_days(year + 1, @new_year_starting_month, @new_year_starting_day)
-        first_day_next_year - first_day
+        last_iso_day_of_year(year) - first_iso_day_of_year(year) + 1
       end
 
       # Here we use month to mean ordinal month. Therefore if the calendar
@@ -128,23 +126,20 @@ defmodule Cldr.Calendar.Julian.Compiler do
         adjusted_month =
           Cldr.Math.amod(ordinal_month + @new_year_starting_month - 1, @months_in_year)
 
-        adjusted_year =
-          if adjusted_month < @new_year_starting_month, do: year + 1, else: year
-
         cond do
           # The first month of the year will be short since the year starts
           # part way through the month
           adjusted_month == @new_year_starting_month ->
             days_in_julian_month = Cldr.Calendar.Julian.days_in_month(year, ordinal_month)
-            days_in_julian_month - @new_year_starting_day
+            days_in_julian_month - @new_year_starting_day + 1
 
           # The last month of the year will be "long" since the first part of the
           # first month that is before the start of year will be included
           adjusted_month == @last_month_of_year ->
             start_of_month =
-              date_to_iso_days(adjusted_year, adjusted_month, 1)
+              date_to_iso_days(year, adjusted_month, 1)
             start_of_next_month =
-              date_to_iso_days(adjusted_year, @new_year_starting_month, @new_year_starting_day)
+              date_to_iso_days(year + 1, @new_year_starting_month, @new_year_starting_day)
             start_of_next_month - start_of_month
 
           true ->
@@ -153,11 +148,10 @@ defmodule Cldr.Calendar.Julian.Compiler do
       end
 
       def year(year) do
-        first_day = date_to_iso_days(year, @new_year_starting_month, @new_year_starting_day)
-        {:ok, first_date} = Date.new(year, @new_year_starting_month, @new_year_starting_day, __MODULE__)
+        {year, month, day} = first_day_of_year(year)
+        {:ok, first_date} = Date.new(year, month, day, __MODULE__)
 
-        last_day = date_to_iso_days(year, @new_year_starting_month, @new_year_starting_day) - 1
-        {year, month, day} = date_from_iso_days(last_day)
+        {year, month, day} = last_day_of_year(year)
         {:ok, last_date} = Date.new(year, month, day, __MODULE__)
 
         Date.range(first_date, last_date, 1)
@@ -171,13 +165,10 @@ defmodule Cldr.Calendar.Julian.Compiler do
         adjusted_month =
           Cldr.Math.amod(ordinal_month + @new_year_starting_month - 1, @months_in_year)
 
-        adjusted_year =
-          if adjusted_month < @new_year_starting_month, do: year + 1, else: year
-
         first_day =
           if adjusted_month == @new_year_starting_month, do: @new_year_starting_day, else: 1
 
-        {:ok, first} = Date.new(adjusted_year, adjusted_month, first_day, __MODULE__)
+        {:ok, first} = Date.new(year, adjusted_month, first_day, __MODULE__)
         first_iso_days = date_to_iso_days(year, adjusted_month, first_day)
         days_in_month = days_in_month(year, ordinal_month)
 
@@ -205,20 +196,21 @@ defmodule Cldr.Calendar.Julian.Compiler do
       end
 
       def day_of_year(year, month, day) do
-        adjusted_year =
-          if month < @new_year_starting_month, do: year + 1, else: year
-
-        first_day = date_to_iso_days(year, @new_year_starting_month, @new_year_starting_day)
-        this_day = date_to_iso_days(adjusted_year, month, day)
+        first_day = first_iso_day_of_year(year)
+        this_day = date_to_iso_days(year, month, day)
         this_day - first_day + 1
-      end
-
-      def calendar_year(year, month, day) when year_rollover(month, day) do
-        year - 1
       end
 
       def calendar_year(year, _month, _day) do
         year
+      end
+
+      def extended_year(year, month, day) when year_rollover(month, day) do
+        calendar_year(year, month, day)
+      end
+
+      def cyclic_year(year, month, day) do
+        calendar_year(year, month, day)
       end
 
       def related_gregorian_year(year, month, day) do
@@ -227,20 +219,32 @@ defmodule Cldr.Calendar.Julian.Compiler do
         year
       end
 
-      def extended_year(year, month, day) when year_rollover(month, day) do
-        year - 1
+      def first_day_of_year(year) do
+        month = @new_year_starting_month
+        day = @new_year_starting_day
+        {year, month, day}
       end
 
-      def extended_year(year, month, day) when year_rollover(month, day) do
-        year
+      def last_day_of_year(year) do
+        last_day = first_iso_day_of_year(year + 1) -  1
+        date_from_iso_days(last_day)
       end
 
-      def cyclic_year(year, month, day) do
-        calendar_year(year, month, day)
+      def first_iso_day_of_year(year) do
+        {year, month, day} = first_day_of_year(year)
+        date_to_iso_days(year, month, day)
+      end
+
+      def last_iso_day_of_year(year) do
+        {year, month, day} = last_day_of_year(year)
+        date_to_iso_days(year, month, day)
+      end
+
+      def leap_year?(year) do
+        last_iso_day_of_year(year) -  first_iso_day_of_year(year) + 1 == 366
       end
 
       defdelegate valid_date?(year, month, day), to: Cldr.Calendar.Julian
-      defdelegate leap_year?(year), to: Cldr.Calendar.Julian
       defdelegate week(year, week), to: Cldr.Calendar.Julian
       defdelegate weeks_in_year(year), to: Cldr.Calendar.Julian
       defdelegate months_in_year(year), to: Cldr.Calendar.Julian
