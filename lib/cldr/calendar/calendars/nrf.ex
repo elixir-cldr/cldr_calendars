4766acb14896032c2393bf3f1a05ebaa294672ed
Kip Cole <kipcole9@gmail.com>
2025-09-25 12:31:09 +1000

    Fix NRF and CSCO calendar configuration

--- lib/cldr/calendar/calendars/nrf.ex
+++ lib/cldr/calendar/calendars/nrf.ex
@@ -16,6 +16,7 @@ defmodule Cldr.Calendar.NRF do
   """
   use Cldr.Calendar.Base.Week,
     min_days_in_first_week: 4,
+    begins_or_ends: :ends,
     first_or_last: :last,
     day_of_week: 6,
     month_of_year: 1
--- mix/corporate/csco.ex
+++ mix/corporate/csco.ex
@@ -3,6 +3,7 @@ require Cldr.Calendar.Compiler.Week
 defmodule Cldr.Calendar.CSCO do
   use Cldr.Calendar.Base.Week,
     min_days_in_first_week: 7,
+    begins_or_ends: :ends,
     first_or_last: :last,
     day_of_week: 6,
     month_of_year: 7
